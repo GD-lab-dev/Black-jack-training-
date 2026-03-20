8c36858edff8af6cf7973da0b866de434b9f8da4
Garrett L <langfoga@mail.gvsu.edu>
2026-03-20 14:54:44 -0400

    fixed start.sh again

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
-pip install -r requirements.txt
+if command -v pip3 >/dev/null 2>&1; then
+    pip3 install -r requirements.txt
+else
+    pip install -r requirements.txt
+fi
 
 if command -v python3 >/dev/null 2>&1; then
     python3 new_chat_main.py
